2b49cf2672ca45e10117fece1f6accc42b1b6f0e
Diogo Guedes <diogoworkss@gmail.com>
2022-10-23 22:28:51 -0300

    criação do array contendo os goleiros

---
+++ README.md
@@ -0,0 +1,36 @@
+<h2>Meu Time da Copa</h2>
+
+<img style="width: 50%;" src="https://placar.abril.com.br/wp-content/uploads/2021/09/esporte-copa-taca-20180614-001-1.jpg" alt="taça da copa do mundo">
+
+<h3>Sumário</h3>
+
+<a href="#desc">.Descrição do projeto</a> <br>
+<a href="#status">.Status</a> <br>
+<a href="#func">.Funcionalidades</a> <br>
+<a href="#techs">.Tecnologias utilizadas</a> <br> <br>
+
+
+
+<h3 id="desc">Descrição do projeto</h3><br>
+
+<p> Meu Time da Copa é um game interativo onde o usuário pode testar seus conhecimentos sobre a história das copas e ser recompensado por figurinhas dos craques da Copa do Mundo de 2022, competindo com seus amigos pra ver quem terá o time com mais craques.
+    
+</p> <br> <br>
+
+
+<h3 id="status">Status</h3> <br>
+
+<p>Finalizando</p> <br><br>
+
+
+<h3 id="func">Funcionalidades</h3> <br>
+
+<p>1 - Entreter os amantes do futebol e da Copa do Mundo</p><br>
+<p>2 - Medir os conhecimentos futebolísticos</p><br>
+<p>3 - Se preparar para acompanhar a Copa do Mundo do Catar</p> <br> <br>
+
+<h3 id="techs">Tecnologias utilizadas</h3> <br>
+
+<p>.HTML</p> <br>
+<p>.CSS</p> <br>
+<p>.JavaScript</p> <br>
---
+++ script.js
@@ -0,0 +1,116 @@
+const goleiro = [
+    { 
+        id: 1,
+        nome: 'Ter Stegen',
+        skills: 90,
+        selecao: 'Alemanha',
+        foto: '<img class="res" src="https://img.a.transfermarkt.technology/portrait/big/74857-1599987750.jpg?lm=1" alt="">'
+    },
+
+    { 
+        id: 2,
+        nome: 'Alisson',
+        skills: 91,
+        selecao: 'Brasil',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4yiE97impcCCGZW7Lc_SkdeFmCR5Pd5hp3t9Hym-cPM40kgGw9jx8zplcWQZEg5M-fTk&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 3,
+        nome: 'Lloris',
+        skills: 90,
+        selecao: 'França',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQODwBJNh29b64FJNTN_BMuHnTTwEmIX_T-Q86MAji8lC9W3riWL_wFh5W6IULNPtwPShI&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 4,
+        nome: 'Courtois',
+        skills: 93,
+        selecao: 'Bélgica',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvn-SLuF3gyO6NW2Z_qB4dDyNmihcis4DnDg&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 5,
+        nome: 'Tim Howard',
+        skills: 88,
+        selecao: 'Estados Unidos',
+        foto: '<img class="res" src="https://img.a.transfermarkt.technology/portrait/big/4267-1587066706.jpg?lm=1" alt="">'
+    },
+
+    { 
+        id: 6,
+        nome: 'De Gea',
+        skills: 90,
+        selecao: 'Espanha',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxzqnZjfDIyw-LlymI1T2WULGKBDmOGCS02w&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 7,
+        nome: 'Diogo Costa',
+        skills: 89,
+        selecao: 'Portugal',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtZuRRpHq5w3NpxFWrOztH88pUp1S-_gdFaw&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 8,
+        nome: 'Sommer',
+        skills: 90,
+        selecao: 'Suíça',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJi9hOOqbRkV6HQQrQvMohv750hcYBwqfekg&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 9,
+        nome: 'Pickford',
+        skills: 91,
+        selecao: 'Inglaterra',
+        foto: '<img class="res" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfUhOzlw-f_mKjBiy0rP5AWVS76XbnPUgjYk5D35_zh4yPUYJ90tn89d2G2zRUoJ8AqEA&usqp=CAU" alt="">'
+    },
+
+    { 
+        id: 10,
+        nome: 'Navas',
+        skills: 91,
+        selecao: 'Porto Rico',
+        foto: '<img class="res" src="https://www.realmadrid.com/img/vertical_380px/navascc_20220617070516.jpg"  alt="">'
+    }
+]
+
+const goleiro_sorteado = Math.floor(Math.random() * goleiro.length); 
+
+
+const respostas = ['klose', 'italia']; 
+
+
+function verificar(){
+    var select1 = document.querySelector("#questao1");
+    var opcaoTexto = select1.options[select1.selectedIndex].text;
+    var opcaoValor = select1.options[select1.selectedIndex].value;
+
+    var msgm = document.getElementById('msgm1');
+
+    if (opcaoValor == respostas[0]){
+
+        msgm.innerHTML = ' Parabéns, tu é brabo!<br> O card que você ganhou é <div>'+goleiro[goleiro_sorteado].foto+'</div> <br> Nome: '+goleiro[goleiro_sorteado].nome+'<br> Skills: '+goleiro[goleiro_sorteado].skills+'<br> Seleção: '+goleiro[goleiro_sorteado].selecao;
+    } else {
+        msgm.innerHTML = 'Cara, você é burro?'
+    }
+}
+
+// const select = document.querySelector('#questao1');
+
+// const indice = select.selectedIndex;
+// const valor = select.value;
+// const text = select.options[indice].text;
+
+// const select = document. querySelector("#questao1");
+// const options = [...select.options];
+// const listar = options.forEach((option) => {
+// console.log(option.value);
+// })
+
+
